957704bb30ecec9ed6b82e872099ef553d4b4e91
wangkeke <wang432991>
2020-05-21 10:32:02 +0800

    first init

---
+++ app/src/main/java/com/coo/mvp_fast_coo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.coo.mvp_fast_coo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
